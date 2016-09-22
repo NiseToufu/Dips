2a639f648c27eb97f0535b1ba4edca0363f233f2
NiseToufu <harithnonework@gmail.com>
2016-09-23 02:45:55 +0800

    Yo

---
+++ www/js/app.js
@@ -0,0 +1,60 @@
+// Ionic Starter App
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'yulpApp' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+angular.module('yulpApp', ['ionic'])
+.run(function ($ionicPlatform) {
+ $ionicPlatform.ready(function () {
+ if(window.cordova && window.cordova.plugins.Keyboard) {
+ // Hide the accessory bar by default (remove this to show the accessory bar abovethe keyboard
+ // for form inputs)
+ cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+ // Don't remove this line unless you know what you are doing. It stops the viewport
+ // from snapping when text inputs are focused. Ionic handles this internally for
+ // a much nicer keyboard experience.
+ cordova.plugins.Keyboard.disableScroll(true);
+ }
+ if(window.StatusBar) {
+ StatusBar.styleDefault();
+ }
+ });
+})
+.config(function ($stateProvider, $urlRouterProvider) {
+ $stateProvider
+ .state('home', {
+ url: '/home',
+ templateUrl: 'views/home/home.html',
+ abstract: true
+ })
+
+ .state('home.feed', {
+ url: '/feed', // url will be /home/feed
+ views: {
+ 'tab-feed': {
+ templateUrl: 'views/home/feed.html'
+ }
+ }
+ })
+
+ .state('home.search', {
+ url: '/search', // url will be /home/search
+ views: {
+ 'tab-search': {
+ templateUrl: 'views/home/search.html'
+ }
+ }
+ })
+
+ .state('home.settings', {
+ url: '/settings', // url will be /home/settings
+ views: { // named views
+ 'tab-settings': {
+ templateUrl: 'views/home/settings.html'
+ }
+ }
+ });
+
+
+ // if none of the above states are matched, use this as the fallback
+ $urlRouterProvider.otherwise('/home/feed');
+});
